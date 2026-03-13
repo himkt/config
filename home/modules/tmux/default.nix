6bcd2dd492ddc020b362882914f137fa4714f050
himkt <himkt@klis.tsukuba.ac.jp>
2026-03-13 18:44:02 +0900

    refactor: move tmux package from home.packages to mise

--- home/modules/tmux/default.nix
+++ home/modules/tmux/default.nix
@@ -1,15 +1,5 @@
+{ ... }:
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-
-{
-  home.packages = with pkgs; [
-    tmux
-  ];
-
   xdg.configFile."tmux/tmux.conf" = {
     source = ./files/tmux.conf;
   };
